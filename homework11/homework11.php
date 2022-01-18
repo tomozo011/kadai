10494e1006373165a427eb5f5749f5ac145c011a
tomozo011 <tomohiroi591@gmail.com>
2022-01-18 10:47:05 +0900

    PHP 1/18-3

--- homework11/homework11.php
+++
@@ -1,8 +0,0 @@
-<boby>
-    <?php
-        echo("Hello!"."<br>");
-        echo("My name is Tomohiro.". "<br>");
-        echo("I live in Wakayama.". "<br>");
-        echo("Thank You!");
-    ?>
-</boby>
