be6b6ac8d5d733affbaacd8d7468219645abdfdf
tomozo011 <97370548+tomozo011@users.noreply.github.com>
2022-01-18 11:12:32 +0900

    homework11

---
+++ homework11/homework11.php
@@ -0,0 +1,8 @@
+<boby>
+    <?php
+        echo("Hello!"."<br>");
+        echo("My name is Tomohiro.". "<br>");
+        echo("I live in Wakayama.". "<br>");
+        echo("Thank You!");
+    ?>
+</boby>
